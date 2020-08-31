ee3a0eb95a4ba9a36495d5107a6b46db627a3b85
jushita <jushitaa@gmail.com>
2020-08-31 16:29:38 -0500

    added material-ui

--- plant-collection/src/components/footer/footer.tsx
+++ plant-collection/src/components/footer/footer.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Toolbar from '@material-ui/core/Toolbar';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Typography from '@material-ui/core/Typography';
+import useStyles from '../app/app-styles';
+import AppBar from '@material-ui/core/AppBar';
+
 
 export default function Footer() {
+    const classes = useStyles();
     return (
-        <div>This is footer</div>
+        <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Album layout
+          </Typography>
+        </Toolbar>
+      </AppBar>
     )
 }
--- plant-collection/src/components/header/header.tsx
+++ plant-collection/src/components/header/header.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 export default function Header(): JSX.Element {
     return (
-        <div>Nav Bar</div>  
+        <Button variant="contained" color="primary">
+            Hello World
+        </Button>  
     )
 }
