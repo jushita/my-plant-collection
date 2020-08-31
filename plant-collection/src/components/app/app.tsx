0aa85f3fbe593a55cfb8beb47c7809fb00af3ac8
jushita <jushitaa@gmail.com>
2020-08-31 15:42:42 -0500

    Main added

--- plant-collection/src/components/app/app.tsx
+++ plant-collection/src/components/app/app.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from '../header';
-
+import Main from '../main'
 
 export default (
     
     function App() {
 
         return (
+            <>
             <Header></Header>
+            <Main></Main>
+            </>
         )
     }
 )
