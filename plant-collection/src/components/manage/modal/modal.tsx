4faeda733b216a14585654f0ff5a31bec6e46002
jushita <jushitaa@gmail.com>
2020-09-25 15:07:06 -0500

    modal component added

---
+++ plant-collection/src/components/manage/modal/modal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './modal.css';
+
+export default function Modal() {
+    console.log(`here in modal`)
+    return (
+        <div>
+            HIi
+        </div>
+    )
+
+}
